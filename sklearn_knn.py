f49377794ece5482039f670bab677eb4d1fe8346
Mark <925283731@qq.com>
2018-06-06 19:54:50 +0800

    sklearn 代码提交

---
+++ sklearn_knn.py
@@ -0,0 +1,31 @@
+# coding:utf-8
+'''''
+Created on 2016/4/24
+@author: Gamer Think
+'''
+# 导入NearestNeighbor包 和 numpy
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+
+# 定义一个数组
+X = np.array([[-1, -1],
+              [-2, -1],
+              [-3, -2],
+              [1, 1],
+              [2, 1],
+              [3, 2]
+              ])
+""" 
+NearestNeighbors用到的参数解释 
+n_neighbors=5,默认值为5，表示查询k个最近邻的数目 
+algorithm='auto',指定用于计算最近邻的算法，auto表示试图采用最适合的算法计算最近邻 
+fit(X)表示用X来训练算法 
+"""
+nbrs = NearestNeighbors(n_neighbors=3, algorithm="ball_tree").fit(X)
+# 返回距离每个点k个最近的点和距离指数，indices可以理解为表示点的下标，distances为距离
+distances, indices = nbrs.kneighbors(X)
+print indices
+print distances
+
+neighbors_array = nbrs.kneighbors_graph(X).toarray()
+print neighbors_array
